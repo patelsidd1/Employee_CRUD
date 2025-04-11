1b9eebb83e0278dd99ab9f1a68a43e15bcfaf7f4
Siddhant Patel <patelsiddhant29@gmail.com>
2025-04-11 18:29:03 +0530

    11/04

--- src/test/java/com/Config/Employee/EmployeeApplicationTests.java
+++ src/test/java/com/Config/Employee/EmployeeApplicationTests.java
@@ -3,11 +3,17 @@ package com.Config.Employee;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class EmployeeApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+		String user;
+		user = "abcd";
+		assertEquals("abcd", user);
 	}
 
 }
